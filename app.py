70bde454d76d7297e993f2e43dd6a82313c99dc2
Prajakta Singhal <singhal.riya2018@gmail.com>
2026-03-14 00:10:27 +0530

    Day 3: Added interactive business dashboards with Plotly

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd 
+import plotly.express as px
 from analysis import analyze_sales
 
 st.title("AI Powered Business Insights Dashboard")
@@ -14,7 +15,46 @@ if uploaded_file:
     
     results = analyze_sales(df)
 
-    st.subheader("Business Insights")
+    st.subheader("Key Business Metrics")
 
-    st.write("Total Sales:", results["total_sales"])
-    st.write("Best Selling Product:", results["top_product"])
+    col1, col2 = st.columns(2)
+
+    col1.metric("Total Sales:", results["total_sales"])
+    col2.metric("Best Selling Product:", results["top_product"])
+
+    st.subheader("Sales by Region")
+    
+    region_df = results["region_sales"].reset_index()
+    region_df.columns = ['Region', 'Sales']
+
+    fig1 = px.bar(region_df, x = 'Region', y = 'Sales', title="Sales by Region", color='Region')
+
+    st.plotly_chart(fig1)
+
+    st.subheader("Monthly Sales Trend")
+
+    month_df = results["monthly_sales"].reset_index()
+    month_df.columns = ['Month', 'Sales']
+
+    fig2 = px.line(month_df, x='Month', y='Sales', title="Monthly Sales Trend", markers=True)
+
+    st.plotly_chart(fig2)
+
+    st.subheader("Product Performance")
+    
+    product_sales = df.groupby('product')['sales'].sum().reset_index()
+
+    fig3 = px.bar(product_sales, x='product', y='sales', title="Product Sales Comparison", color='product')
+
+    st.plotly_chart(fig3)
+
+st.markdown(
+    """
+    <hr>
+    <div style="text-align: right; font-size: 14px;">
+        Designed & Developed by Prajakta Singhal
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
